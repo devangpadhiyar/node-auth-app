8fb3ba26496388060cc1a177db27578dd233b7de
Devang Padhiyar <devangpadhiyar700@gmail.com>
2019-03-07 17:10:26 +1100

    Initial Project setup for node.js with authentication setup

---
+++ app.js
@@ -0,0 +1,58 @@
+import * as config from "./config";
+
+const createError = require('http-errors');
+const express = require('express');
+const passport = require("passport");
+const session = require('express-session');   // express sessions for session middleware
+const RedisStore = require('connect-redis')(session);  // Redis store for sessions
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+
+// session engine setup
+app.use(session({
+    store: new RedisStore({
+        url: config.redisStore.url
+    }),
+    secret: config.redisStore.secret,
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ config/index.js
@@ -0,0 +1,8 @@
+const config = {};
+
+config.redisStore = {
+    url: "",
+    secret: "HELLO world"
+};
+
+module.exports = config
